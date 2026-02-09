e9976dab4c0871325c0b389e4763d4c0fb9015dd
Allie Hedgewing <technological102@gmail.com>
2026-02-09 11:30:36 -0500

    designed command utility revised

---
+++ src/test/TopSecretTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TopSecretTest {
+
+    @Test
+    void numberFiles() {
+    }
+
+    @Test
+    void pathToString() {
+    }
+
+    @Test
+    void checkCommandArgument() {
+    }
+
+    @Test
+    void main() {
+    }
+}
