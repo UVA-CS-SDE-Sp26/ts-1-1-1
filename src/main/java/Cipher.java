ad97f124e00b4713a2123327af88586ddd7f64ef
khokharkola <alif.khokhar17@gmail.com>
2026-02-10 01:21:15 -0500

    I think the first commit failed, so attempt 2 at trying to commit and push the files I wrote as Team Member D (Ali)

--- src/main/java/Cipher.java
+++ src/main/java/Cipher.java
@@ -1,4 +1,104 @@
-package PACKAGE_NAME;
+//Ali Khokhar - Team 1.1
+//Team Member D
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
+import java.util.*;
 
 public class Cipher {
+
+    //instance variables
+    String keyFileName;
+    File keyFile;
+    String normalLetters;
+    String cipheredLetters;
+    ArrayList<String> linesOfKeyFile = new ArrayList<>();
+    HashMap<Character, Character> keyLettersByIndex = new HashMap<>();
+
+    //constructor
+    public Cipher(String keyFileNameInput) {
+        keyFileName = keyFileNameInput;
+        keyFile = new File(this.keyFileName);
+
+        //resetting if this object was used before
+        normalLetters = "";
+        cipheredLetters = "";
+        linesOfKeyFile.clear();
+        keyLettersByIndex.clear();
+
+        //build the Cipher object so that it's ready for deciphering
+        this.getCipherLines();
+        this.createHashMap();
+    }
+
+    //methods
+
+    //method to get the lines from the key file
+    public void getCipherLines() throws IllegalArgumentException {
+
+        try
+        {
+            Scanner scan = new Scanner(keyFile);
+
+            while (scan.hasNextLine())
+            {
+                linesOfKeyFile.add(scan.nextLine());
+            }
+
+            //validating the key: making sure there are two lines
+            if(linesOfKeyFile.size() != 2) {
+                throw new IllegalArgumentException("There are more or less than 2 lines");
+            }
+            else
+            {
+                normalLetters = linesOfKeyFile.get(0);
+                cipheredLetters = linesOfKeyFile.get(1);
+            }
+
+            scan.close();
+        }
+        catch (FileNotFoundException e)
+        {
+            throw new IllegalArgumentException("File not found");
+        }
+
+        //validating the key: making sure the two lines are equal (one to one)
+        if(normalLetters.length() != cipheredLetters.length()) {
+            throw new IllegalArgumentException("The lines in the key do not match length");
+        }
+
+        //validating the key: making sure no letter gets mapped to two different places
+        if(cipheredLetters.length() != cipheredLetters.chars().distinct().count()) {
+            throw new IllegalArgumentException("One letter is being mapped/ciphered to more than one letter");
+        }
+
+    }
+
+    //method to create HashMap
+    public void createHashMap() {
+        for(int i = 0; i < normalLetters.length(); i++) {
+            keyLettersByIndex.put(cipheredLetters.charAt(i), normalLetters.charAt(i));
+        }
+    }
+
+    //method to finally decipher an inputted String using the HashMap
+    public String decipherStringInput(String input) {
+        StringBuilder decipheredString = new StringBuilder();
+
+        for(int i = 0; i < input.length(); i++)
+        {
+            if(keyLettersByIndex.containsKey(input.charAt(i)))
+            {
+                decipheredString.append(keyLettersByIndex.get(input.charAt(i)));
+            }
+            else
+            {
+                decipheredString.append(input.charAt(i));
+            }
+        }
+
+        return decipheredString.toString();
+    }
+
 }
--- src/test/java/CipherTest.java
+++ src/test/java/CipherTest.java
@@ -1,4 +1,166 @@
-package PACKAGE_NAME;
+//Ali Khokhar - Team 1.1
+//Team Member D
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CipherTest {
+
+    //helper method to create test files
+    private void createTestFiles(String fileName, String line1, String line2) throws IOException {
+        FileWriter writer = new FileWriter(fileName);
+        writer.write(line1);
+        writer.write("\n" + line2);
+        writer.close();
+    }
+    //helper method to create test files with only one line
+    private void createTestFiles(String fileName, String line1) throws IOException {
+        FileWriter writer = new FileWriter(fileName);
+        writer.write(line1);
+        writer.close();
+    }
+
+    //Tests for the method getCipherLines
+    @Test
+    void getCipherLinesTypicalUsage() {
+        try {
+            createTestFiles("testfile1.txt", "ABC", "CBA");
+        }
+        catch (IOException e) {
+            System.out.println("IO Exception");
+        }
+
+        Cipher c = new Cipher("testfile1.txt");
+
+        assertEquals('A', c.keyLettersByIndex.get('C'), "Should output A as the matching letter for C");
+    }
+
+    @Test
+    void getCipherLinesShortSecondLine() {
+        try {
+            createTestFiles("testfile2.txt", "ABC", "CB");
+        }
+        catch (IOException e) {
+            System.out.println("IO Exception");
+        }
+
+        assertThrows(IllegalArgumentException.class, () -> {new Cipher("testfile2.txt");}, "Should throw illegal argument error");
+    }
+
+    @Test
+    void getCipherLinesMissingSecondLine() {
+        try {
+            createTestFiles("testfile3.txt", "ABC");
+        }
+        catch (IOException e) {
+            System.out.println("IO Exception");
+        }
+
+        assertThrows(IllegalArgumentException.class, () -> {new Cipher("testfile3.txt");}, "Should throw illegal argument error");
+    }
+
+    @Test
+    void getCipherLinesDuplicateCipherLetters() {
+        try {
+            createTestFiles("testfile4.txt", "ABC", "CBB");
+        }
+        catch (IOException e) {
+            System.out.println("IO Exception");
+        }
+
+        assertThrows(IllegalArgumentException.class, () -> {new Cipher("testfile4.txt");}, "Should throw illegal argument error");
+    }
+
+    @Test
+    void getCipherLinesFileNotFound() {
+        try {
+            createTestFiles("testfile4.txt", "ABC", "CBB");
+        }
+        catch (IOException e) {
+            System.out.println("IO Exception");
+        }
+
+        assertThrows(IllegalArgumentException.class, () -> {new Cipher("fileNotFound.txt");}, "Should throw illegal argument error");
+    }
+
+
+    //tests for the createHashMap method
+    @Test
+    void createHashMapTypicalUsage() {
+        try {
+            createTestFiles("testfile5.txt", "ABC", "CBA");
+        }
+        catch (IOException e) {
+            System.out.println("IO Exception");
+        }
+
+        Cipher c = new Cipher("testfile5.txt");
+
+        assertTrue(c.keyLettersByIndex.containsValue('A'), "Should say True");
+        assertTrue(c.keyLettersByIndex.containsValue('B'), "Should say True");
+        assertTrue(c.keyLettersByIndex.containsValue('C'), "Should say True");
+        assertTrue(c.keyLettersByIndex.containsValue('C'), "Should say True");
+        assertTrue(c.keyLettersByIndex.containsValue('B'), "Should say True");
+        assertTrue(c.keyLettersByIndex.containsValue('A'), "Should say True");
+    }
+
+    @Test
+    void createHashMapWrong() {
+        try {
+            createTestFiles("testfile6.txt", "ABC", "CBA");
+        }
+        catch (IOException e) {
+            System.out.println("IO Exception");
+        }
+
+        Cipher c = new Cipher("testfile6.txt");
+
+        assertFalse(c.keyLettersByIndex.containsValue('D'), "Should say True");
+        assertFalse(c.keyLettersByIndex.containsValue('E'), "Should say True");
+        assertFalse(c.keyLettersByIndex.containsValue('F'), "Should say True");
+        assertFalse(c.keyLettersByIndex.containsValue('F'), "Should say True");
+        assertFalse(c.keyLettersByIndex.containsValue('E'), "Should say True");
+        assertFalse(c.keyLettersByIndex.containsValue('G'), "Should say True");
+    }
+
+    //Tests for deciphering method
+    @Test
+    void decipherStringInputTypicalUsage() {
+        try {
+            createTestFiles("testfile7.txt", "NOO", "YES");
+        }
+        catch (IOException e) {
+            System.out.println("IO Exception");
+        }
+
+            Cipher c = new Cipher("testfile7.txt");
+
+            assertEquals("NOO", c.decipherStringInput("YES"), "Should output NOO");
+    }
+
+    @Test
+    void decipherStringInputWithSpaces() {
+        try {
+            createTestFiles("testfile7.txt", "NOO", "YES");
+        }
+        catch (IOException e) {
+            System.out.println("IO Exception");
+        }
+
+        Cipher c = new Cipher("testfile7.txt");
+
+        assertEquals("NOO ONOO", c.decipherStringInput("YES EYES"), "Should output NOO");
+    }
+
 }
