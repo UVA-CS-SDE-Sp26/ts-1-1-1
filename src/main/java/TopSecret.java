60757ecbc3b1923fa18d2b9a8ae32edf154551ad
Gyeom <anp2qe@virginia.edu>
2026-02-11 21:22:57 -0500

    TopSecret

--- src/main/java/TopSecret.java
+++ src/main/java/TopSecret.java
@@ -1,20 +1,7 @@
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.List;
-import java.util.ArrayList;
-
-import java.util.stream.Stream;
-
-/**
- * Commmand Line Utility
- */
 public class TopSecret {
 
     public static void main(String[] args) {
-        ProgramControl commandHandler = new ProgramControl();
-        commandHandler.commandUtility(args);
+        UserInterface userinterface = new UserInterface();
+        userinterface.run(args);
     }
 }
