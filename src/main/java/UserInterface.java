fec0fb556ee926893413f9775971a595b8d6a3cf
Gyeom <anp2qe@virginia.edu>
2026-02-11 21:19:51 -0500

    UserInterface

---
+++ src/main/java/UserInterface.java
@@ -0,0 +1,41 @@
+import java.util.List;
+
+public class UserInterface {
+
+    public void run(String[] args) {
+            //ProgramControl program = new ProgramControl();
+            FileHandler filehandler = new FileHandler();
+            try{
+                List<String> fileList = ProgramControl.ArrayOfFiles(filehandler);
+                if (args.length == 0) {
+                    printFiles(fileList);
+                } else if (args.length < 3) {
+                        int fileNum = ProgramControl.checkCommandArgument(args[0]);
+                        String cipherKey = "key.txt"; // default key
+                        if (args.length == 2) {
+                            cipherKey = (args[1]);
+                        }
+                        String content = filehandler.readFile(numToFileName(fileNum,fileList));
+                        Cipher cipher = new Cipher(cipherKey);
+                        System.out.println(cipher.decipherStringInput(content));
+                } else {
+                        System.out.println("Up to 2 arguments allowed: fileNumber keyFile");
+                }
+            }
+            catch (Exception e){
+                System.out.println("error: " + e.getMessage());
+            }
+
+    }
+
+    private void printFiles(List<String> list) {
+        for (String line : list) {
+            System.out.println(line);
+        }
+    }
+
+    private String numToFileName(int num, List<String> list){
+        return list.get(num-1);
+    }
+
+}
